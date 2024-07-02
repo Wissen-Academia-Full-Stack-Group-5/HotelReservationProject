9ba560f73eb618efc5b65a2f959aaa65f239f07e
Fırat Gültekin <frt.gltkn131@gmail.com>
2024-07-02 12:29:38 +0300

    repos and interfaceses updated. add view model and services

---
+++ Service/Services/HotelService.cs
@@ -0,0 +1,27 @@
+﻿using Entity.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services
+{
+	public class HotelService
+	{
+		public Task Add(HotelViewModel model)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<HotelViewModel> Get(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<HotelViewModel>> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
