dcc3ead99464a4cec25e21e9b910e4144a9e1731
Fırat Gültekin <frt.gltkn131@gmail.com>
2024-07-03 16:16:08 +0300

    Fail UnitOfWork

---
+++ Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,57 @@
+﻿using Data.Contexts;
+using Data.Repositories;
+using Entity.Repositories;
+using Entity.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWorks
+    {
+        private readonly HotelDbContext _context;
+        
+        private bool _disposed = false;
+
+        public UnitOfWork(HotelDbContext context)
+        {
+            _context = context;
+            
+        }
+        public IRepository<T> GetRepository<T>() where T : class, new()
+        {
+            return new Repository<T>(_context);
+        }
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public virtual void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+    }
+}
