ae10ba8ad6025b80ebf1d69de6f6c71a16bacb0a
Fırat Gültekin <frt.gltkn131@gmail.com>
2024-07-17 11:57:15 +0300

    -Modelde CheckInDate ve CheckOutDate nullable (DateTime?) olarak güncellendi.
    -Index.cshtml'de tarih alanları placeholder ile güncellendi.

--- Entity/ViewModels/SearchViewModel.cs
+++ Entity/ViewModels/SearchViewModel.cs
@@ -8,8 +8,8 @@ namespace Entity.ViewModels
 {
     public class SearchViewModel
     {
-        public DateTime CheckInDate { get; set; }
-        public DateTime CheckOutDate { get; set; }
+        public DateTime? CheckInDate { get; set; }
+        public DateTime? CheckOutDate { get; set; }
         public string City { get; set; }
         public string Type { get; set; }
         public List<string> Cities { get; set; }
